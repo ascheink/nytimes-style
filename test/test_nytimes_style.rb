20f6cd84f340a7e11ccf6ea839fa17cfbf823636
Andrei <andrei@nytimes.com>
2011-06-09 14:54:50 -0400

    fixed order of arguments in tests

--- test/test_nytimes_style.rb
+++ test/test_nytimes_style.rb
@@ -5,29 +5,29 @@ class NytimesStyleTest < Test::Unit::TestCase
 
   def test_dates
     date = Date.civil(2001, 9, 11)
-    assert_equal nytimes_month_and_day(date), "Sept. 11"
-    assert_equal nytimes_date(date), "Sept. 11, 2001"
+    assert_equal "Sept. 11", nytimes_month_and_day(date)
+    assert_equal "Sept. 11, 2001", nytimes_date(date)
     assert_raise(ArgumentError) { nytimes_month(0) }
   end
   
   def test_date_options
     date = Date.civil(2001, 9, 11)
-    assert_equal nytimes_date(date, :day_of_week => true), "Tuesday, Sept. 11, 2001"
-    assert_equal nytimes_date(date, :hide_current_year => true), "Sept. 11, 2001"
+    assert_equal "Tuesday, Sept. 11, 2001", nytimes_date(date, :day_of_week => true)
+    assert_equal "Sept. 11, 2001", nytimes_date(date, :hide_current_year => true)
     this_year = Date.today.year
     recent_date = Date.civil(this_year, 2, 12)
-    assert_equal nytimes_date(recent_date, :hide_current_year => true), "Feb. 12"
+    assert_equal "Feb. 12", nytimes_date(recent_date, :hide_current_year => true)
   end
   
   def test_state_abbrevs
-    assert_equal nytimes_state_abbrev('AZ'), 'Ariz.'
-    assert_equal nytimes_state_abbrev('New Hampshire'), 'N.H.'
+    assert_equal 'Ariz.', nytimes_state_abbrev('AZ')
+    assert_equal 'N.H.',  nytimes_state_abbrev('New Hampshire'), 
     assert_raise(ArgumentError) { nytimes_state_abbrev('Canada') }
   end
   
   def test_numbers
-    assert_equal nytimes_number(5), 'five'
-    assert_equal nytimes_number(12), '12'
+    assert_equal 'five', nytimes_number(5)
+    assert_equal '12', nytimes_number(12)
   end
   
 end
