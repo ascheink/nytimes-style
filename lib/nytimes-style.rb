38ecdc64190607cbfa5b6d11d286f3f3430c13da
Jeff Larson <thejefflarson@gmail.com>
2012-08-30 12:36:24 -0400

    Add in the ability to use subclasses of DateTime or Time in nytimes_time, e.g. so that ActiveSupport::TimeWithZone works

--- lib/nytimes-style.rb
+++ lib/nytimes-style.rb
@@ -5,11 +5,11 @@ require 'yaml'
 # hosted on [Github](https://github.com/ascheink/nytimes-style).
 module Nytimes
   module Style
-    
-    # > "In general, spell out the first nine cardinal and 
+
+    # > "In general, spell out the first nine cardinal and
     # > ordinal numbers [but] spell any number that begins a sentence..."
-    # Exceptions include "ages of people 
-    # and animals," "sums of money," "degrees of temperature" and "mentions of the Twelve 
+    # Exceptions include "ages of people
+    # and animals," "sums of money," "degrees of temperature" and "mentions of the Twelve
     # Apostles and the Ten Commandments."
     def nytimes_number(n)
       if n < 10
@@ -30,21 +30,21 @@ module Nytimes
       str << ", #{date.year}" unless opts[:hide_current_year] && date.year == Date.today.year
       return str
     end
-    
+
     def nytimes_month_and_day(date)
       "#{nytimes_month date.month} #{date.day}"
     end
-    
+
     def nytimes_month(month)
       raise ArgumentError.new "Unknown month: #{month}" unless (1..12).include? month
       %w(Jan. Feb. March April May June July Aug. Sept. Oct. Nov. Dec.)[month - 1]
     end
-    
+
     # > "Use numerals in giving clock time: 10:30 a.m.; 10:30.
     # > Do not use half-past 10 except in a direct quotation.
     # > Also avoid the redundant 10:30 a.m. yesterday morning and Monday afternoon at 2 p.m."
     def nytimes_time(time, opts={})
-      raise ArgumentError.new "Time or DateTime required" unless time.class == DateTime || time.class == Time
+      raise ArgumentError.new "Time or DateTime required" unless time.is_a?(DateTime) || time.is_a?(Time)
       str = ""
       str << time.strftime("%l:%M").strip
       str << time.strftime(" %p").sub('PM','p.m.').sub('AM','a.m.') unless opts[:hide_abbreviation]
@@ -61,13 +61,13 @@ module Nytimes
       raise ArgumentError.new "Unknown postal code, state name or FIPS code: #{state_name_or_code}" unless state
       state['nytimes_abbrev']
     end
-    
+
     def nytimes_state_name(state_abbrev_or_code)
       state = states[state_abbrev_or_code]
       raise ArgumentError.new "Unknown postal code, abbreviation or FIPS code: #{state_abbrev_or_code}" unless state
       state['name']
     end
-    
+
     private
 
     STATE_DATA_FILE = File.join(File.dirname(__FILE__), 'nytimes-style/states.yml')
@@ -77,7 +77,7 @@ module Nytimes
         h.merge({ state['postal_code'] => state, state['name'] => state, state['fips_code'] => state })
       end
     end
-    
+
 
   end
 end
